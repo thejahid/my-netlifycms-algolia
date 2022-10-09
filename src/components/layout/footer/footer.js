94dcfa2e62926d3a7734fe79c6609fb8d39722c6
Jahid Hasan <jhpabel@gmail.com>
2022-10-09 21:17:26 +0600

    Update footer.js

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
     return (
         <Fragment>
             <footer className='footer mt-auto py-3'>
-                <div className='text-center copy'>©developerjahid , 2020. All rights reserved. Website by <a target="_blank" rel="noopener noreferrer" href="https://developerjahid.com">@developerjahid.</a></div>
+                <div className='text-center copy'>©thejahid , 2020. All rights reserved. Website by <a target="_blank" rel="noopener noreferrer" href="https://thejahid.me">@thejahid
+        .</a></div>
             </footer>
         </Fragment>
     )
